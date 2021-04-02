166254e826df8701ff5c8a582e60abdbc6877e3c
Dario Nuñez <76497304+dnunez-cloud@users.noreply.github.com>
2021-04-02 20:36:58 -0300

    js files added, css files folder added, html files added

---
+++ js/register.js
@@ -0,0 +1,129 @@
+var validations = document.querySelector('.validations-hidden');
+var errors = ['Validation results:'];
+
+var formExists = function() {
+  if (document.querySelector('#registerForm')) {
+    return true;
+  }
+  else {
+    errors.push(' Form is not found');
+  }
+}
+formExists();
+
+var inputsNumbers = function () {
+  var inputs = document.querySelectorAll('input');
+  if (inputs.length == 6) {
+    return true;
+  }
+  else {
+    errors.push(' There are missing inputs');
+  }
+}
+inputsNumbers()
+
+var labels = function () {
+  var input = document.querySelectorAll('.input');
+  var label = document.querySelectorAll('.label');
+  var inputLabel;
+  for (var i = 0; i < input.length; i++) {
+    for (var j = 0; j < label.length; j++) {
+      if (input[i].getAttribute('type') == label[j].getAttribute('for')) {
+        inputLabel = true;
+        j = label.length;
+      }
+      else {
+        inputLabel = false;
+      }
+    }
+    if (inputLabel == false) {
+      break;
+    }
+  }
+  if (inputLabel == true) {
+    return true;
+  }
+  else {
+    errors.push(' There are Labels missing for inputs');
+  }
+}
+labels();
+
+var required = function () {
+  var inputs = document.querySelectorAll('.input');
+  var inputRequired;
+  for (var i = 0; i < inputs.length; i++){
+    if (inputs[i].hasAttribute('required')) {
+      inputRequired = true;
+    }
+    else {
+      inputRequired = false;
+      break;
+    }
+  }
+  if (inputRequired == true) {
+    return true;
+  }
+  else {
+    errors.push(' Required attributes missing');
+  }
+}
+required();
+
+var anchor = function () {
+  var anchor = document.querySelector('.linkRegister');
+  if (anchor.getAttribute('href') == 'login.html') {
+    return true;
+  }
+  else {
+    errors.push(' Anchor tag is invalid');
+  }
+}
+anchor();
+
+var buttonExists = function() {
+  if (document.querySelector('.inputButton')) {
+    return true;
+  }
+  else {
+    errors.push(' Missing submit button');
+  }
+}
+buttonExists();
+
+var buttonContent = function() {
+  var button = document.querySelector('.inputButton');
+  if (button.getAttribute('value') == 'Sign Up') {
+    return true;
+  }
+  else {
+    errors.push('Buttons content is wrong');
+  }
+}
+buttonContent();
+
+var buttonContentReset = function() {
+  var button = document.querySelector('.inputButtonReset');
+  if (button.getAttribute('value') == 'Reset') {
+    return true;
+  }
+  else {
+    errors.push('Buttons content is wrong');
+  }
+}
+buttonContentReset();
+
+var validateAll = function () {
+  var validationsHidden = document.querySelector('.validations-hidden');
+  validationsHidden.className = 'validations';
+  if (formExists() == true && inputsNumbers() == true && labels() == true && required() == true && anchor() == true && 
+  buttonContent() == true && buttonContentReset() == true && buttonExists() == true) {
+    validations.style.backgroundColor= 'green';
+    validations.textContent =  'Every validation has passed!';
+  }
+  else { 
+    validations.style.backgroundColor = 'red';
+    validations.textContent = errors;
+  }
+}
+validateAll();
